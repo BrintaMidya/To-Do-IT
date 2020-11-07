170af33443867371b7d7af33c76dfdbc5115a66c
BrintaMidya <brintamidya@gmail.com>
2020-11-07 20:33:25 +0530

    Local Storage Feature Added

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,39 +1,140 @@
+//========================================================================================
+//========================== VARIABLE AND FUNCTION DECLARATION ===========================
+//========================================================================================
+
+let todoList = [];
+
+const getData = () => {
+	const reference = localStorage.getItem("todos");
+	if (reference) {
+		todoList = JSON.parse(reference);
+	}
+};
+
+const storeData = () => {
+	localStorage.setItem("todos", JSON.stringify(todoList));
+};
+
+const renderData = () => {
+	const doneList = document.querySelector("#done");
+	const notDoneList = document.querySelector("#notDone");
+
+	doneList.innerHTML = "";
+	notDoneList.innerHTML = "";
+
+	todoList.forEach(function (item) {
+		const newLi = document.createElement("li");
+		newLi.setAttribute("data-key", item.id);
+		newLi.innerHTML =
+			"<span><img src='./Assets/graphics/clear.svg' alt='delete'></span>" +
+			item.text;
+		if (item.done) {
+			doneList.append(newLi);
+		} else {
+			notDoneList.append(newLi);
+		}
+	});
+};
+
+const deleteData = (id) => {
+	todoList = todoList.filter(function (item) {
+		return item.id != id;
+	});
+	storeData();
+};
+
+//========================================================================================
+//============================ INITIALIZING AND DATA LOADING =============================
+//========================================================================================
+
+getData();
+renderData();
+
+//========================================================================================
+//=============================== SETTING EVENT HANDLERS =================================
+//========================================================================================
+
 //Adding new tasks
 $("input[type='text']").keypress(function (event) {
 	if (event.which === 13) {
 		var txt = $(this).val();
-		$(this).val("");
-		$("#notDone").append(
-			"<li><span><img src='./Assets/graphics/clear.svg' alt='delete'></span>" +
-				txt +
-				"</li>"
-		);
+		if (txt != "") {
+			$(this).val("");
+			const newTodo = {
+				id: Date.now(),
+				text: txt,
+				done: false,
+			};
+			todoList.push(newTodo);
+			storeData();
+			const newLi = document.createElement("li");
+			newLi.setAttribute("data-key", newTodo.id);
+			newLi.innerHTML =
+				"<span><img src='./Assets/graphics/clear.svg' alt='delete'></span>" +
+				newTodo.text;
+			$("#notDone").append(newLi);
+		}
 	}
 });
 
 //Removing a task
 $("ul").on("click", "span", function (event) {
+	var selectedId;
 	$(this)
 		.parent()
 		.fadeOut(200, function () {
+			selectedId = $(this).attr("data-key");
 			$(this).remove();
+			deleteData(selectedId);
 		});
 	event.stopPropagation();
 });
 
 //Moving a task from notDone to Done list
 $("#notDone").on("click", "li", function () {
-	temp = $(this).html();
+	id = $(this).attr("data-key");
+	txt = $(this).text();
+
 	$(this).fadeOut(150, function () {
-		$("#done").append("<li>" + temp + "</li>");
+		todoList.forEach(function (item) {
+			if (item.id == id) {
+				item.done = true;
+			}
+		});
+
+		$(this).remove();
+
+		const newLi = document.createElement("li");
+		newLi.setAttribute("data-key", id);
+		newLi.innerHTML =
+			"<span><img src='./Assets/graphics/clear.svg' alt='delete'></span>" + txt;
+		$("#done").append(newLi);
+
+		storeData();
 	});
 });
 
 //Moving a task from Done to notDone list
 $("#done").on("click", "li", function () {
-	temp = $(this).html();
+	id = $(this).attr("data-key");
+	txt = $(this).text();
+
 	$(this).fadeOut(150, function () {
-		$("#notDone").append("<li>" + temp + "</li>");
+		todoList.forEach(function (item) {
+			if (item.id == id) {
+				item.done = false;
+			}
+		});
+
+		$(this).remove();
+
+		const newLi = document.createElement("li");
+		newLi.setAttribute("data-key", id);
+		newLi.innerHTML =
+			"<span><img src='./Assets/graphics/clear.svg' alt='delete'></span>" + txt;
+		$("#notDone").append(newLi);
+
+		storeData();
 	});
 });
 
@@ -53,6 +154,10 @@ $("ul").on("mouseenter", "li", function () {
 $("ul").on("mouseleave", "li", function () {
 	$(this).toggleClass("textHighlight");
 });
+
+//========================================================================================
+//=============================== DATE, TIME AND WEEKDAY =================================
+//========================================================================================
 
 //Displaying Date, WeekDay and TIme
 var date = new Date();
